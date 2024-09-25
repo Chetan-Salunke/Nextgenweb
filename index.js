1e61e41b75e5b8d1dc7f0378d048d119b563153c
Chetan-Salunke <chetanbusalunke@gmail.com>
2024-09-25 11:50:58 +0530

    plans-pricing

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('webpagebtn').addEventListener('click', function() {
+    window.open('web-page.html', '_self');
+});
+
+document.getElementById('planbtn1').addEventListener('click', function() {
+    window.open('plans-pricing.html', '_self');
+});
+
+document.getElementById('planbtn2').addEventListener('click', function() {
+    window.open('plans-pricing.html', '_self');
+});
+
+document.getElementById('planbtn3').addEventListener('click', function() {
+    window.open('plans-pricing.html', '_self');
+});
